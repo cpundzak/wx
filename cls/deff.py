8b4c51f5ddc8f5582ddfb194c441490a2b508f77
cpundzak <chris@cmptoaction.net>
2024-11-11 20:16:30 -0600

    import CSV

--- cls/deff.py
+++ cls/deff.py
@@ -1,4 +1,25 @@
+from cls import setget as s 
+def load():
+    with open('csv/'+'2024-11-07.csv') as f:
+        for line in f:
+            w = line.split(",")
+            wx1 = s.wSetGet(w[0],fil(w[1]),w[2],w[3],w[4],w[5],w[6],w[7],w[8],w[9],w[10],w[11])
+    return wx1
+
 def fil(a):
     w = a[1:]
     t = w 
     return t
+
+def getMax(wx1):
+    a = []
+    for x in range(0,wx1.getLen()):
+        a.append(wx1.getOutTemp(x))
+    return max(a)
+
+def getMin(wx1):
+    a = []
+    for x in range(0,wx1.getLen()):
+        a.append(wx1.getOutTemp(x))
+    return min(a)
+
--- readwxdata.py
+++ readwxdata.py
@@ -1,10 +1,8 @@
-from cls import setget as s 
 from cls import deff as d
 
 
-with open('csv/'+'2024-11-07.csv') as f:
-    for line in f:
-        w = line.split(",")
-        wx1 = s.wSetGet(w[0],d.fil(w[1]),w[2],w[3],w[4],w[5],w[6],w[7],w[8],w[9],w[10],w[11])
-for x in range(0,wx1.getLen()):
-    print(wx1.getOutTemp(x))
+w = d.load()
+
+print(d.getMax(w))
+
+print(d.getMin(w))
