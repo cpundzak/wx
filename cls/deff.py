b5034390be28d9baa6aa746933265cf9325282da
cpundzak <chris@cmptoaction.net>
2024-11-12 18:24:51 -0600

    Modify Load Statement

--- cls/deff.py
+++ cls/deff.py
@@ -1,7 +1,12 @@
 from cls import setget as s 
-def load():
-    with open('csv/'+'2024-11-07.csv') as f:
+def load(d,st):
+    masterAr = []
+    with open('csv/'+d+'.csv') as f:
         for line in f:
+            w = line.split(",")
+            masterAr.append(w[0]+","+w[1]+","+w[2]+","+w[3]+","+w[4]+","+w[5]+","+w[6]+","+w[7]+","+w[8]+","+w[9]+","+w[10]+","+w[11])
+        for x in range(0, len(masterAr), st):
+            line = masterAr[x]
             w = line.split(",")
             wx1 = s.wSetGet(w[0],fil(w[1]),w[2],w[3],w[4],w[5],w[6],w[7],w[8],w[9],w[10],w[11])
     return wx1
--- readwxdata.py
+++ readwxdata.py
@@ -1,8 +1,8 @@
 from cls import deff as d
+from cls import setget as s
 
-
-w = d.load()
-
+w = d.load("2024-10-25",30)
 print(d.getMax(w))
-
 print(d.getMin(w))
+
+
